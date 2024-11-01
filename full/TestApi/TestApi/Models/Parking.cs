d5375bb3af54492bfefd71f5921741feba593256
Life <geravanya963@gmail.com>
2024-11-01 19:05:16 +0300

    full add

---
+++ full/TestApi/TestApi/Models/Parking.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TestApi.Entities;
+
+namespace TestApi.Models
+{
+    public class ResponseParking
+    {
+        public ResponseParking(Parking parking)
+        {
+            ParkingId = parking.ParkingId;
+            Name = parking.Name;
+            Address = parking.Address;
+            HourRate = parking.HourRate;
+        }
+
+        public int ParkingId { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public decimal? HourRate { get; set; }
+    }
+}
