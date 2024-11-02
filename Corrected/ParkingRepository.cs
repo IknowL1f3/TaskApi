50f8d259df396c74f9666538b3305938e4153473
Life <geravanya963@gmail.com>
2024-11-02 22:01:01 +0300

    return files

---
+++ Corrected/ParkingRepository.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using TestApi.Entities;
+
+namespace TestApi.BDLvl
+{
+    public class ParkingRepository
+    {
+        private ParkingEntities5 db = new ParkingEntities5();
+
+        public List<Parking> GetAllParkings()
+        {
+            return db.Parking.ToList();
+        }
+        public Parking GetParkingById(int id)
+        {
+            return db.Parking.Find(id);
+        }
+
+        public void AddParking(Parking parking)
+        {
+            db.Parking.Add(parking);
+            db.SaveChanges();
+        }
+
+        public void UpdateParking(Parking parking)
+        {
+            db.Entry(parking).State = EntityState.Modified;
+            db.SaveChanges();
+        }
+
+        public void RemoveParking(int id)
+        {
+            var parking = db.Parking.Find(id);
+            if (parking != null)
+            {
+                db.Parking.Remove(parking);
+                db.SaveChanges();
+            }
+        }
+
+        public bool ParkingExists(int id)
+        {
+            return db.Parking.Count(e => e.ParkingId == id) > 0;
+        }
+    }
+}
---
+++ full/TestApi/TestApi/BLL/ParkingBLL.cs
@@ -0,0 +1,58 @@
+﻿using FluentValidation;
+using FluentValidation.Results;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TestApi.BDLvl;
+using TestApi.Entities;
+using TestApi.Models;
+using TestApi.Validator;
+
+namespace TestApi.BLL
+{
+    public class ParkingBLL
+    {
+        private ParkingRepository _repository = new ParkingRepository();
+        private ParkingValidator _validator = new ParkingValidator();
+
+        public List<ResponseParking> GetAllParkings()
+        {
+            return _repository.GetAllParkings().ConvertAll(p => new ResponseParking(p));
+        }
+
+        public Parking GetParking(int id)
+        {
+            return _repository.GetParkingById(id);
+        }
+
+        public void AddParking(Parking parking)
+        {
+            if (parking == null)
+                return;
+
+            ValidationResult result = _validator.Validate(parking);
+            if (!result.IsValid)
+                throw new ValidationException(result.Errors);
+
+            _repository.AddParking(parking);
+        }
+
+        public void UpdateParking(Parking parking)
+        {
+            if (parking == null)
+                return;
+
+            ValidationResult result = _validator.Validate(parking);
+            if (!result.IsValid)
+                throw new ValidationException(result.Errors);
+
+            _repository.UpdateParking(parking);
+        }
+
+        public void RemoveParking(int id)
+        {
+            _repository.RemoveParking(id);
+        }
+    }
+}
